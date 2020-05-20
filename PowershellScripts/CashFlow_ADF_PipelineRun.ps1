cb79604321368c11e0b10cfb28e70e7b009f5d27
Azerz <Azerz@trg.com>
2020-05-20 10:05:24 -0400

    initial PS for end to cash flow data factory execution.

---
+++ PowershellScripts/CashFlow_ADF_PipelineRun.ps1
@@ -0,0 +1,75 @@
+param
+(
+    #path to json file to save the azure profile. 
+    #This prevents for entering Azure login details every time script is run
+    [Parameter(Mandatory=$true)]
+    [string] $Env = "DEV"
+)
+
+if ($Env -eq "DEV"){
+   #Set environment variables
+$resourceGroupName="rg_NPRD-DEV"
+$dataFactoryName = "e2nprddfcf"
+#ADF Pipeline to run the end to end data load from Miosoft output to Asbestos DM
+$pipelineName= "CashFlow_Datamart_End_to_End" #EntAsbestos_Datamart_End_to_End
+$outputFile = "D:\Powershell\Output\Output.txt"
+# Connect to Azure using Active Directory Service Principal Authentication 
+$passwd = Get-Content "D:\Powershell\Creds\azure_cred.properties" | ConvertTo-SecureString -AsPlainText -Force
+$pscredential = New-Object System.Management.Automation.PSCredential('9f713ae4-f576-4ccb-96db-9654b2c907c1', $passwd)
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant e93ade6c-2138-454c-87ec-7353d64d3cbb
+}
+try
+{
+
+#Run the Data Factory Pipeline
+$runId = Invoke-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -PipelineName $pipelineName
+
+Write-Host $runId -ForegroundColor DarkYellow
+#Checking the Status of the pipeline
+while($true)
+{
+    $result = Get-AzDataFactoryV2PipelineRun -DataFactoryName $dataFactoryName -ResourceGroupName $resourceGroupName -PipelineRunId $runId 
+    if (($result | Where-Object { $_.Status -eq "InProgress" } | Measure-Object).count -ne 0) 
+        {
+                Write-Host "Pipeline run status: In Progress " -foregroundcolor "Yellow"
+                $pipelineName + " run status: In Progress at " + (get-date -Format MM-dd-yyyy_HH_mm_ss) -join "`r`n"` | Out-File $outputFile -Append
+                Start-Sleep -Seconds 360
+        }
+    else 
+        {
+                Write-Host "Pipeline '"$pipelineName"' run finished. Result:" -foregroundcolor "Green" 
+                $pipelineName + " run status: Succeded at " + (get-date -Format MM-dd-yyyy_HH_mm_ss) -join "`r`n"` | Out-File $outputFile -Append
+                "Result:" -join "`r`n"` | Out-File $outputFile -Append
+                $result >> $outputFile
+                break
+        }        
+}
+
+}
+catch
+{
+    $problem = $true
+    $errormessage = $_.Exception.Message 
+}
+finally
+{
+    if ($problem)
+    {
+    
+        Write-Host $errorMessage
+        $errorMessage -join "`r`n"` | Out-File $outputFile -Append
+    }
+    else
+    {
+        if ($result.Status -eq "Succeeded")
+        {   
+            'Entire process is completed' + -join "`r`n"` | Out-File $outputFile -Append
+            Exit 
+        }
+        else
+        {
+            $result.Error -join "`r`n"` | Out-File $outputFile -Append 
+            EXit
+        }
+    }
+}
