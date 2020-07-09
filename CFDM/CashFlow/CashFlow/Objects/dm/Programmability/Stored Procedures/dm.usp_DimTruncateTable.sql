bf3da9bcf5bde6c69eaf566df5ffe9a9a78c49d7
Azerz <Azerz@trg.com>
2020-07-09 14:37:18 -0400

    Updated the truncate proc, fixed a bug that was trying to trying truncate a view

--- CFDM/CashFlow/CashFlow/Objects/dm/Programmability/Stored Procedures/dm.usp_DimTruncateTable.sql
+++ CFDM/CashFlow/CashFlow/Objects/dm/Programmability/Stored Procedures/dm.usp_DimTruncateTable.sql
@@ -34,7 +34,8 @@ SET
 			INFORMATION_SCHEMA.TABLES
 		WHERE
 			TABLE_SCHEMA = @schema
-			AND TABLE_NAME NOT IN ('FactCashflow', 'Date', 'Reinsurance','FactCashflow_org') FOR XML PATH(''),
+			AND TABLE_NAME NOT IN ('FactCashflow', 'Date', 'Reinsurance','FactCashflow_org') 
+			AND TABLE_TYPE = 'BASE TABLE' FOR XML PATH(''),
 			TYPE
 	).value('.', 'NVARCHAR(MAX)')
 END
@@ -47,7 +48,8 @@ SET
 			INFORMATION_SCHEMA.TABLES
 		WHERE
 			TABLE_SCHEMA = @schema
-			AND TABLE_NAME <> 'DimDate' FOR XML PATH(''),
+			AND TABLE_NAME <> 'DimDate' 
+			AND TABLE_TYPE = 'BASE TABLE' FOR XML PATH(''),
 			TYPE
 	).value('.', 'NVARCHAR(MAX)')
 END --select @sql
